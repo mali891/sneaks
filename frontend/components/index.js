236bbbba92202a54d03cda84f7ff43e4016d91a3
Mo Ali <m.ali23@hotmail.co.uk>
2020-06-22 20:28:45 +0100

    added base styles

---
+++ frontend/components/index.js
@@ -0,0 +1,7 @@
+import Page from './Page'
+import ErrorMessage from './ErrorMessage'
+import Header from './Header'
+import Nav from './Nav'
+import Meta from './Meta'
+
+export { Page, ErrorMessage, Header, Nav, Meta }
---
+++ frontend/pages/sell.js
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+
+const Sell = props => (
+  <div>
+    <h1>Sell</h1>
+    <Link href="/">Sell</Link>
+  </div>
+)
+
+export default Sell
